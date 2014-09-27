98cc060cba7fc9d9203081202aae9f4e6a3e40e8
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2014-09-27 21:04:14 +0000

    Wrote compute_skip() and compute_branch().
    They seem to be working fine.

--- proj1/LCtrie.c
+++ proj1/LCtrie.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 #define ADDR_LEN	32	// address length in bits
 #define LINE_LEN	ADDR_LEN + 6	// 1 whitespace char, max 3 interface digits, \n\0
+#define DISCARD_VAL	UINT_MAX	// interface value for discarded packets
 
 #define DEBUG
 
@@ -16,7 +18,7 @@ typedef struct node{
 
 typedef struct fib_entry{
 	unsigned prefix;
-	char prefixlen;
+//	char prefixlen;
 	unsigned nexthop;
 } fib_entry;
 
@@ -30,7 +32,8 @@ void memerr(void *p)
 	}
 }
 
-unsigned binstrtoi(char *str)
+/* binary string to unsigned pushed left. example on 8 bits 101 -> 10100000 */
+unsigned binstrtoi_l(char *str)
 {
 	short n;
 	unsigned nr=0;
@@ -40,7 +43,8 @@ unsigned binstrtoi(char *str)
 		if(str[n]=='1')
 			nr |= 1<<(sizeof(unsigned)*8-1-n);
 	}
-	return nr>>(sizeof(unsigned)*8-n);
+	return nr;
+	/* return nr>>(sizeof(unsigned)*8-n);	for _r */
 }
 
 typedef int (*cmpfn)(const void*, const void*);
@@ -52,6 +56,64 @@ int fib_prefix_cmp(fib_entry *a, fib_entry *b)
 		return -1;
 	return 0;
 }
+
+
+/* --------  -------- */
+
+/* sets to 0 the n highest bits of nr */
+unsigned clear_pre(unsigned nr, unsigned short n)
+{
+	return nr << n >> n;
+}
+
+/* extracts from nr the first n bits starting at position p (p=0 -> MSB) */
+unsigned extract(unsigned nr, unsigned short p, unsigned short n)
+{
+	return nr << p >> (sizeof(unsigned)*8 - n);
+}
+
+/* returns the skip value possible for the n entries in fib starting at index first. ignores the first pre bits. it is assumed all entries in the fib are different */
+unsigned short compute_skip(fib_entry *fib, unsigned short pre, unsigned first, unsigned n)
+{
+	unsigned first_pre, last_pre;	// first and last prefixes in the interval
+	short p;
+
+/*	first_cle = clear_pre(fib[first].prefix, pre);
+	last_cle = clear_pre(fib[first+n-1].prefix, pre);*/
+
+	first_pre = fib[first].prefix;
+	last_pre = fib[first+n-1].prefix;
+
+	p = pre;
+	while(extract(first_pre, p, 1) == extract(last_pre, p, 1))
+		p++;
+
+	return p - pre;
+}
+
+/* returns the branch value possible for the n entries in fib starting at index first. ignores the first pre bits. */
+unsigned short compute_branch(fib_entry *fib, unsigned short pre, unsigned first, unsigned n)
+{
+	short branch, m;
+	unsigned i;
+
+	if(n==2)
+		return 1;
+
+	for(branch=2, i=0; i<n; branch++)
+	{
+		for(i=0, m=0; m<((1<<branch)-1) && i<n; i++)
+		{
+			//printf("%d\t%d\t%d\n", branch, i, m);
+			if(extract(fib[first + i].prefix, pre, branch) == m)
+				m++;
+
+		}
+	}
+	return branch-2;
+}
+
+
 
 /* -------- MAIN -------- */
 
@@ -71,15 +133,15 @@ int main(int argc, char **argv)
 	}
 
 	char line[LINE_LEN];
-	unsigned empty_nexthop;	// empty prefix interface
+	unsigned empty_nexthop=0;	// empty prefix interface
 
 /* verify first line of file has empty prefix */
 
 	fgets(line, LINE_LEN, fp);
-	if(sscanf(line, "* %u", &empty_nexthop) != 1)
+	if(sscanf(line, "* %u", &empty_nexthop) != 1)	// no next hop defined for empty prefix. choose a value to signal that packets should be discarded
 	{
-		printf("First line of '%s' is not the empty prefix.\n\n", argv[1]);
-		exit(0);
+		empty_nexthop = DISCARD_VAL;
+		printf("First line of '%s' is not the empty prefix. Some packets may be discarded.\n", argv[1]);
 	}
 
 /* load the FIB into memory */
@@ -96,14 +158,19 @@ int main(int argc, char **argv)
 	}
 
 	#ifdef DEBUG
-	printf("the FIB has %d entries\n", nrlines);
+	printf("the FIB has %d entries (excluding empty prefix)\n", nrlines);
 	#endif
 
 	fib = malloc(nrlines*sizeof(fib_entry));
 	memerr(fib);
 
 	rewind(fp);
-	fgets(line, LINE_LEN, fp);	// skip the line with the empty prefix
+	if(empty_nexthop != DISCARD_VAL)	// BUG if empty nexthop is set to discard_val in the file
+	{
+		fgets(line, LINE_LEN, fp);	// skip the line with the empty prefix
+	}
+	else
+		nrlines++;	// the first line had a prefix which we did not count
 
 	i = 0;
 	while(fgets(line, LINE_LEN, fp) != NULL)
@@ -114,16 +181,16 @@ int main(int argc, char **argv)
 			exit(0);
 		}
 
-		fib[i].prefix = binstrtoi(prefix_str);
-		fib[i].prefixlen = strlen(prefix_str);
+		fib[i].prefix = binstrtoi_l(prefix_str);
+//		fib[i].prefixlen = strlen(prefix_str);
 		fib[i].nexthop = nexthop;
 		i++;
 	}
 
 	#ifdef DEBUG
-	puts("pref\tlen\tnexthop");
+	puts("pref\t\tnexthop");
 	for(i=0; i<10 && i < nrlines; i++)
-		printf("%d\t%d\t%d\n", fib[i].prefix, fib[i].prefixlen, fib[i].nexthop);
+		printf("%u\t%d\n", fib[i].prefix, fib[i].nexthop);
 	puts("...");
 	#endif
 
@@ -132,9 +199,9 @@ int main(int argc, char **argv)
 	qsort(fib, nrlines, sizeof(fib_entry), (cmpfn)fib_prefix_cmp);
 
 	#ifdef DEBUG
-	puts("pref\tlen\tnexthop");
+	puts("pref\t\tnexthop");
 	for(i=0; i<10 && i< nrlines; i++)
-		printf("%d\t%d\t%d\n", fib[i].prefix, fib[i].prefixlen, fib[i].nexthop);
+		printf("%u\t%d\n", fib[i].prefix, fib[i].nexthop);
 	puts("...");
 	#endif
 
@@ -143,6 +210,12 @@ int main(int argc, char **argv)
 	#ifdef DEBUG
 	printf("each node takes %ld Bytes to store\n\n", sizeof(node));
 	#endif
+
+
+printf("%hu\n", compute_skip(fib, 0, 0, nrlines));
+
+printf("%hu\n", compute_branch(fib, 0, 0, nrlines));
+
 
 
 
