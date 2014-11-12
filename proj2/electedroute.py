7c2ab0504b46b04a995fca30ca2038a7ef19f02a
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2014-11-12 02:04:26 +0000

    Completely redesigned findroute() for great speed.
    Objective accomplished. Some more testing could be useful.

--- proj2/electedroute.py
+++ proj2/electedroute.py
@@ -32,18 +32,16 @@ def loadgraph():
 		relation = int(tmp[2])
 
 		if head not in graph:	# let us add a new node
-			graph[head] = {1:[], 2:[], 3:[]}	# 1 - providers, 2 - peers, 3 - costumers
-		graph[head][relation].append([tail, False])	# add tail to head's relations and initialise edge as unused
+			graph[head] = {1:set(), 2:set(), 3:set(), "visited":False, "via":[0,0]}	# 1 - providers, 2 - peers, 3 - costumers
+		graph[head][relation].add(tail)	# add tail to head's relations and initialise edge as unused
 
 	f.close()	# close the file
 
 def initgraph():
 	""" resets all edges to unused state """
 	global graph
-	for nodeedges in graph.values():
-		for edges in nodeedges.values():
-			for edge in edges:
-				edge[1] = False
+	for node in graph.values():
+		node['visited'] = False
 
 def prompt():
 	""" prompts user to provide an origin and a destination """
@@ -67,107 +65,69 @@ def prompt():
 		exit()
 	return (orig, dest)
 
-def findroutes(orig, dest, path, prev, prev_rel):
-	""" finds routes from orig to dest and saves them in a global
-		variable routes. prev is the previous node used to reach
-		orig. path is the path until prev. prev_rel is the relation
-		between prev and orig from orig's point of view """
-	global routes
 
-	if len(routes) > 0 and orig in routes[0]:
-		route_aux = list(routes[0])
-		for i, node in enumerate(route_aux):
-			if node == orig:
-				routes.append(path + route_aux[i:])
-				routes = [electroute()]	# find which route is best: the new or the current
-				break	# all done with this route
+def findroute(orig, dest):
+	""" finds routes from orig to dest and returns that route as a list.
+		returns None if no route exists connecting the nodes """
+	global graph	
 
-	# add current node to path
-	path.extend([orig])
-	#print path
+	fringe = set()
+	fringe.add(orig)
+	graph[orig]['visited'] = True
+	graph[orig]['via'] = [0, 1]	# orig reached via provider
+	nrnodes = len(graph)
+	newfringe = set()
 
-	# check if this node is the destination
-	if orig == dest:
-		routes.append(path)	# add path to routes
-		routes = [electroute()]	# find which route is best: the new or the current
-		return
+	print "nr nodes: " + str(nrnodes)
+	while fringe:	# while fringe is not empty
+		newfringe.clear()
+		for node in fringe:
+			if graph[node]['via'][1] == 1:
+				explore = [1, 2, 3]
+			else:
+				explore = [3]
 
-	if prev_rel == 3 or prev_rel == 2:	# this node is either a costumer of prev or a peer of prev. use only costumers
-		explore = [3]
-	elif prev_rel == 1: # this node is a provider of prev. explore any node
-		explore = [3, 2, 1]	# the order is important. explore costumers first. prevents loops
-
-	# backup current edges state for this node for use bellow
-	bak = copy.deepcopy(graph[orig])
-
-	# mark all edges with correct relation from this node as used. this prevents using this node again
-	for relation in explore:
-		for i, (node, used) in enumerate(graph[orig][relation]):
-			graph[orig][relation][i][1] = True	# mark outgoing edge as used
-
-	# follow unused edges
-	for relation in explore:
-		for node, used in bak[relation]:
-			if used == False and node != prev:	# unused edge which does not go to prev
-				findroutes(node, dest, list(path), orig, relation)
-
-def neighbour_rel(node, tosearch):
-	""" finds the relation node has to tosearch """
-	for relation, neighbours in graph[node].items():
-		for neighbour in neighbours:
-			if neighbour[0] == tosearch:
-				return relation
+			for relation in explore:
+				for neighbour in graph[node][relation]:
+					if graph[neighbour]['visited'] == False:
+						graph[neighbour]['visited'] = True
+						graph[neighbour]['via'] = [node, relation]
+						newfringe.add(neighbour)
+						if neighbour == dest:
+							print "found dest"
+							route = []
+							node = dest
+							while node != orig:
+								route = [node] + route
+								node = graph[node]['via'][0]
+							return [orig] + route
+		fringe = set(newfringe)
 	return None
 
-def electroute():
-	""" finds the elected route from a list of valid routes.
-		returns that route as a list """
-	str_routes = []
-
-	# map a route into its relations from the point of view of the destination
-	# example: if origin traverses only clients (33...33) the destinantion will see a path of providers (11...11)
-	for route in routes:
-		str_routes.append(''.join(str(neighbour_rel(route[i], route[i-1])) for i in range(1, len(route))))
-
-	# sort routes. most prefered first, that is sort alphabetically and in length
-	ordered_routes = [y for (x,y) in sorted(zip(str_routes, routes))]
-	#print sorted(str_routes)
-	#print ordered_routes
-	if not ordered_routes:	# list is empty
-		return None
-	return ordered_routes[0]
 
 def test_policy_connection():
-	global routes
 
-	tier1 = []
+	tier1 = set()
 	for node in graph:
 		if not graph[node][1]:
-			tier1.append(node)
+			tier1.add(node)
 			
-	print tier1
 	for node in tier1:
-		if not set(tier1).issubset(set([i for (i, j) in graph[node][2]] + [node])):
-			print "The graph is NOT policy connected, at least " + str(node) + " cannot connect to some nodes."
+		if not tier1 <= graph[node][2] | set([node]):
+			print "The graph is NOT policy connected. At least " + str(node) + " cannot connect to some nodes."
 			return
-		#graph[node][2].remove(node)
 	print "The graph is policy connected."
 
 
 check_args()
 loadgraph()
-routes = []
 test_policy_connection()
 #pprint.pprint(graph)
 print "Press Return twice to exit."
 while True:
 	orig, dest = prompt()
-	routes = []
 	initgraph()
-	findroutes(orig, dest, [], None, 1)
-	print len(routes)
 	#print routes
-	print "The elected route is " + str(routes[0] if len(routes) > 0 else None)
-	#print "The elected route is " + '-'.join(map(str, routes[0] if len(routes) > 0 else [None]))
+	print "The elected route is " + str(findroute(orig, dest))
 	#pprint.pprint(graph)
 
